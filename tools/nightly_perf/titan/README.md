40f521881b0f7d45bb5b99ecc06fe7c88bc1b927
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-10-23 15:49:29 -0400

    nightly: Titan setup is working, at least.

---
+++ tools/nightly_perf/titan/README.md
@@ -0,0 +1,9 @@
+# Setup
+
+ 1. Setup SSH key
+ 2. ```
+    git clone -b master https://github.com/StanfordLegion/legion.git
+    cd legion/tools/nightly_perf/titan
+    ./setup.sh
+    ```
+ 3. Create file `legion/tools/nightly_perf/titan/env.sh` (defines `PERF_ACCESS_TOKEN`)
--- tools/nightly_perf/titan/setup.sh
+++ tools/nightly_perf/titan/setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
 root_dir="$(dirname "${BASH_SOURCE[0]}")"
-cd "${root_dir}"
-echo root dir is ${root_dir}
+
+module unload PrgEnv-pgi
+module load PrgEnv-gnu
+module load python
+
+export CC=gcc
+export CXX=CC
+export HOST_CC=gcc
+export HOST_CXX=g++
+export USE_GASNET=1
+export CONDUIT=gemini
+export RDIR=auto
 
 unset LG_RT_DIR
-USE_GASNET=1 RDIR=auto CONDUIT=gemini CC=gcc CXX=CC HOST_CC=gcc HOST_CXX=g++ ./setup_env.py
-##RDIR=auto CONDUIT=gemini CC="cc -dynamic" CXX="CC -dynamic" HOST_CC="cc -dynamic" HOST_CXX="CC -dynamic" ./setup_env.py
+
+"$root_dir/../../../language/scripts/setup_env.py" --prefix="$root_dir"
